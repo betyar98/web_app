5a7d80a481e0ac74370fed3cb8003208dbeec9ba
Laci <benilaszlo98@gmail.com>
2023-04-05 10:57:48 +0200

    Initial commit

--- functions.py
+++ functions.py
@@ -1,17 +1,17 @@
-FILEPATH = "feladatok.txt"
+FILEPATH = "todos.txt"
 
 
 def get_todos(filepath=FILEPATH):
     """Beolvassa a dokumentumot és visszaad egy listát a feladatokról."""
     with open(filepath, "r") as file_local:
-        feladatok_local = file_local.readlines()
-    return feladatok_local
+        todos_local = file_local.readlines()
+    return todos_local
 
 
-def write_todos(feladatok_arg, filepath=FILEPATH):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Kiírja a feladatok listáját egy dokumentumba."""
     with open(filepath, "w") as file_local:
-        file_local.writelines(feladatok_arg)
+        file_local.writelines(todos_arg)
 
 
 # ez csak akkor fut le ha az program neve main pl. ha importáljuk másik fájlba akkor már nem az a neve
